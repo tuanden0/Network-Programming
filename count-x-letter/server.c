40dadd1dc8e56238af8e2014c3b6fa0847c14a35
tuanden0 <doanhtuan96@yahoo.com>
2018-05-06 13:23:45 +0700

    Count 'x' letter
    Write a client-server program to do the following actions:
    Client sends a string
    Server replies the number of occurrences of character ‘x’ in that string

---
+++ count-x-letter/server.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[]){
+	/* TCP */
+	/* Variables */
+	int sock, newsock;
+	struct sockaddr_in server;
+	char buffer[1024];
+	int n;
+	/* Socket */
+	sock=socket(AF_INET,SOCK_STREAM,0);
+	server.sin_family=AF_INET;
+	server.sin_addr.s_addr=INADDR_ANY;
+	server.sin_port=htons(5000);		
+	/* Bind */
+	bind(sock,(struct sockaddr*)&server,sizeof(server));
+	/* Listen */
+	listen(sock,5);
+	bzero(&server,sizeof(server));
+	/* Accept */
+	newsock=accept(sock,(struct sockaddr *)0,0);
+	while(1){
+		int count=0,flag=0;
+		bzero(&buffer, sizeof(buffer));
+		n=read(newsock, buffer,sizeof(buffer));
+		printf("Client: %s", buffer);
+		if(n==0){
+			printf("Client disconnected.\n");
+			break;
+		}
+		while(buffer[flag]!='\0'){
+			if(buffer[flag]=='x'){
+				count++;
+			}
+			flag++;
+		}
+		bzero(&buffer, sizeof(buffer));
+		sprintf(buffer,"%d",count); /* convert int to string */
+		write(newsock, buffer, sizeof(buffer));
+	}
+	close(newsock);
+	return 0;
+}
