a63d18b30ac46d3a6c1794f01c2826a97f062b48
tuanden0 <doanhtuan96@yahoo.com>
2018-06-17 12:32:10 +0700

    Transfer Content File
    Write a client/server program to transfer a text file from the server to its client.
    For example: 
     Client sends: “abc.txt”
     Server sends the content of abc.txt to its client, or “File abc.txt does not exist”

---
+++ Transfer content file/server.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[]){
+	/* TCP */
+	/* Variables */
+	int sock, newsock;
+	struct sockaddr_in server;
+	char buffer[1024];
+	int n;
+	/* Socket */
+	sock=socket(AF_INET,SOCK_STREAM,0);
+	server.sin_family=AF_INET;
+	server.sin_addr.s_addr=INADDR_ANY;
+	server.sin_port=htons(5000);		
+	/* Bind */
+	bind(sock,(struct sockaddr*)&server,sizeof(server));
+	/* Listen */
+	listen(sock,5);
+	bzero(&server,sizeof(server));
+	/* Accept */
+	newsock=accept(sock,(struct sockaddr *)0,0);
+	while(1){
+		int c,a,i=0;
+		FILE *file;
+		char content[1024];
+		bzero(&buffer, sizeof(buffer));
+		n=read(newsock, buffer,sizeof(buffer));
+		printf("Client: %s", buffer);
+		if(n==0){
+			printf("Client disconnected.\n");
+			break;
+		}
+		a=strlen(buffer)-1;
+		buffer[a]='\0';
+		file=fopen(buffer, "r");
+		if(file){
+			printf("Content of file: ");
+			while((c=getc(file))!=EOF){
+				content[i]=putchar(c);
+				i++;
+			}
+			fclose(file);
+			bzero(&buffer, sizeof(buffer));
+			strcpy(buffer,content);
+			write(newsock, buffer, sizeof(buffer));
+		} else {
+			bzero(&buffer, sizeof(buffer));
+			strcpy(buffer,"File doesn't exist\n");
+			write(newsock, buffer, sizeof(buffer));
+		}
+	}
+	close(newsock);
+	return 0;
+}
